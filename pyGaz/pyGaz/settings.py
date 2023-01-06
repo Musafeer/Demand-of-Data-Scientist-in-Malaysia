b11ac4ea1dd7bc4de142050adf91f0edd77bd40f
subaash <subaash@pop-os.localdomain>
2023-01-07 00:20:29 +0800

    added *

--- pyGaz/pyGaz/settings.py
+++ pyGaz/pyGaz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i#kec3^u(_nvkg2$twb_sqhucr(j%k#szxyod2xr%&@!!6oe4&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
