dbbb817c5e1d4e4f59a868181668b92876f923a5
subaash <subaash@pop-os.localdomain>
2023-01-07 00:22:36 +0800

    added static root in settings.py

--- pyGaz/pyGaz/settings.py
+++ pyGaz/pyGaz/settings.py
@@ -127,3 +127,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pydashboard/static'),]
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, "staticfiles"))
